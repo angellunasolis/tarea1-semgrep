882e985ac992e42f930a344fe4398d4c21cf83f9
Angel Luna Solis <angellunaiphone@gmail.com>
2026-03-22 06:37:00 -0600

    Update HardcodedPasswordDemo.java

--- HardcodedPasswordDemo.java
+++ HardcodedPasswordDemo.java
@@ -2,8 +2,8 @@ import java.sql.*;
 
 public class HardcodedPasswordDemo {
     public static void main(String[] args) throws Exception {
-        String password = "SuperSecreto123";
-        String user = "admin";
+        String password = System.getenv("DB_PASSWORD");
+        String user = System.getenv("DB_USER");
         Connection conn = DriverManager.getConnection(
             "jdbc:mysql://localhost/db", user, password);
     }
