ff07dc0c503bf25ac7f66d80a83d3fc246c6d710
Angel Luna Solis <angellunaiphone@gmail.com>
2026-03-22 05:45:30 -0600

    Create HardcodedPasswordDemo.java

---
+++ HardcodedPasswordDemo.java
@@ -0,0 +1,7 @@
+public class HardcodedPasswordDemo {
+    public void conectar() {
+        String password = "SuperSecreto123";
+        String usuario = "admin";
+        System.out.println("Conectando con: " + usuario + " / " + password);
+    }
+}
