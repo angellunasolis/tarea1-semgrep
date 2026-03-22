be3ce6479c3e014b6b9ab28d591c9960d56efc26
Angel Luna Solis <angellunaiphone@gmail.com>
2026-03-22 06:02:39 -0600

    Update CommandInjectionDemo.java

--- CommandInjectionDemo.java
+++ CommandInjectionDemo.java
@@ -2,9 +2,16 @@ import java.io.*;
 
 public class CommandInjectionDemo {
     public static void main(String[] args) throws Exception {
-        String userInput = args[0];
-        String cmd = "ping " + userInput;
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec(cmd);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Ingresa comando: ");
+        String userInput = reader.readLine();
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec(new String[]{"/bin/sh", "-c", userInput});
+        BufferedReader output = new BufferedReader(
+            new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = output.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 }
