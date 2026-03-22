c97d325005447b2d6d7f80db64aff18d2d9e6039
Angel Luna Solis <angellunaiphone@gmail.com>
2026-03-22 06:35:18 -0600

    Update SqlInjectionDemo.java

--- SqlInjectionDemo.java
+++ SqlInjectionDemo.java
@@ -3,9 +3,12 @@ import java.sql.*;
 public class SqlInjectionDemo {
     public void buscarUsuario(String nombre) throws Exception {
         Connection conn = DriverManager.getConnection(
-            "jdbc:mysql://localhost/db", "root", "root");
-        String query = "SELECT * FROM usuarios WHERE nombre = '" + nombre + "'";
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
+            "jdbc:mysql://localhost/db",
+            System.getenv("DB_USER"),
+            System.getenv("DB_PASSWORD"));
+        String query = "SELECT * FROM usuarios WHERE nombre = ?";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, nombre);
+        ResultSet rs = ps.executeQuery();
     }
 }
